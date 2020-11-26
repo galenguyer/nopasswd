537a32dcec1cc31cb1bca48c6b709bd758ee7712
Galen Guyer <galen@galenguyer.com>
2020-11-26 10:53:07 -0800

    store argon data and retreive information later

--- nopasswd.go
+++ nopasswd.go
@@ -11,6 +11,7 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"strconv"
 	"strings"
 
 	"golang.org/x/crypto/argon2"
@@ -36,48 +37,36 @@ func main() {
 	fmt.Print("-> Enter Password: ")
 	pwinput, _ := reader.ReadString('\n')
 
-	_, argonHash, err := GeneratePassword(config, pwinput)
+	argon, argonHash, err := GeneratePassword(config, pwinput)
 	if err != nil {
 		panic(err)
 	}
 
-	text := "Mandalorian is currently the best DisneyPlus show"
-	key := argonHash
-
-	// Encryption
-	encrypted, _ := AESEncrypt(argonHash, text)
-	err = ioutil.WriteFile("vault.data", []byte(encrypted), 0777)
-	if err != nil {
-		fmt.Println(err)
+	_, err = os.Stat("./vault.data")
+	if os.IsNotExist(err) {
+		// Encryption
+		text := "Mandalorian is currently the best DisneyPlus show"
+		encrypted, _ := AESEncrypt(argonHash, text)
+		err = ioutil.WriteFile("vault.data", []byte(argon+"\n"+encrypted), 0777)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
+
 	// Decryption
 	rawBytes, err := ioutil.ReadFile("vault.data")
 	if err != nil {
 		fmt.Println(err)
 	}
-	ciphertext, err := base64.StdEncoding.DecodeString(string(rawBytes[:]))
+	data := strings.Split(string(rawBytes), "\n")
+	argonData := data[0]
+	rawData := []byte(data[1])
+	nHash, _ := GenerateHash(pwinput, argonData)
+	decrypted, err := AESDecrypt(nHash, rawData)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
-	c, err := aes.NewCipher(key)
-	if err != nil {
-		fmt.Println(err)
-	}
-	gcmDecrypt, err := cipher.NewGCM(c)
-	if err != nil {
-		fmt.Println(err)
-	}
-	nonceSize := gcmDecrypt.NonceSize()
-	if len(ciphertext) < nonceSize {
-		fmt.Println(err)
-	}
-	nonce, encryptedMessage := ciphertext[:nonceSize], ciphertext[nonceSize:]
-	plaintext, err := gcmDecrypt.Open(nil, nonce, encryptedMessage, nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("After decrypting:")
-	fmt.Println(string(plaintext))
+	fmt.Println(decrypted)
 }
 
 // AESEncrypt is used to encrypt a string with a given key
@@ -97,8 +86,30 @@ func AESEncrypt(key []byte, data string) (string, error) {
 	return base64.StdEncoding.EncodeToString(gcm.Seal(nonce, nonce, []byte(data), nil)), nil
 }
 
-func AESDecrypt(key []byte, data []byte) {
-
+// AESDecrypt is used to decrypt AES-encrypted bytes with a given key
+func AESDecrypt(key []byte, data []byte) (string, error) {
+	ciphertext, err := base64.StdEncoding.DecodeString(string(data[:]))
+	if err != nil {
+		panic(err)
+	}
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+	gcmDecrypt, err := cipher.NewGCM(c)
+	if err != nil {
+		panic(err)
+	}
+	nonceSize := gcmDecrypt.NonceSize()
+	if len(ciphertext) < nonceSize {
+		panic(err)
+	}
+	nonce, encryptedMessage := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcmDecrypt.Open(nil, nonce, encryptedMessage, nil)
+	if err != nil {
+		panic(err)
+	}
+	return string(plaintext), nil
 }
 
 // GeneratePassword is used to generate a new password hash for storing and
@@ -114,11 +125,29 @@ func GeneratePassword(c *PasswordConfig, password string) (string, []byte, error
 
 	// Base64 encode the salt and hashed password.
 	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
-	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
 
-	format := "$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s"
-	full := fmt.Sprintf(format, argon2.Version, c.memory, c.time, c.threads, b64Salt, b64Hash)
-	return full, hash, nil
+	format := "$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%d"
+	most := fmt.Sprintf(format, argon2.Version, c.memory, c.time, c.threads, b64Salt, len(hash))
+	return most, hash, nil
+}
+
+func GenerateHash(password string, config string) ([]byte, error) {
+	parts := strings.Split(config, "$")
+	c := &PasswordConfig{}
+	_, err := fmt.Sscanf(parts[3], "m=%d,t=%d,p=%d", &c.memory, &c.time, &c.threads)
+	if err != nil {
+		return []byte(""), err
+	}
+	salt, err := base64.RawStdEncoding.DecodeString(parts[4])
+	if err != nil {
+		return []byte(""), err
+	}
+	keyLen, err := strconv.Atoi(parts[5])
+	if err != nil {
+		return []byte(""), err
+	}
+	hash := argon2.IDKey([]byte(password), salt, c.time, c.memory, c.threads, uint32(keyLen))
+	return hash, nil
 }
 
 // ComparePassword is used to compare a user-inputted password to a hash to see
